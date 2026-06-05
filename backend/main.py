829d33165111ee6932384aa9f925d48614b4f64e
fbinter44 <florian.binter@gmail.com>
2026-06-05 15:09:53 +0200

    fix the url

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 # ---------------------------------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://clean-shot.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://fantasy-biathlon.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
